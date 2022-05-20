e4ecc269d4012c24433f1cae5e7046a391ed75b1
Parks <qparks@uab.edu>
2022-05-19 23:09:25 -0500

    setup folder and app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./Client/Pages/Home"
 
-export default App;
+  function App() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+        <Routes>
+
+           {/*project*/}
+          <Route path="/">
+          <Route index element={<Home />} />
+         </Route>
+
+
+
+
+        </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  
+  export default App;
+

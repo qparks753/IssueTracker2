4991c87a3808b8d2009cc0d5ff69c0b93db8a47a
Parks <qparks@uab.edu>
2022-06-25 15:57:33 -0500

    edit server

--- setproxy.js
+++ setproxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://issuetracker-heroku.herokuapp.com/",
+      target: "http://issuetracker-heroku.herokuapp.com/",
       changeOrigin: true,
     })
   );
